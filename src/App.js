a119441a9db1cc7b74a55d0e3a97373a69dbcdd7
peterlah <peterlah@naver.com>
2023-12-18 10:57:11 +0900

    6.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Button from './Button';
-import styles from "./App.module.css";
+// import Button from './Button';
+// import styles from "./App.module.css";
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log('render');
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!!</h1>
-      <Button text="Continue"/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
